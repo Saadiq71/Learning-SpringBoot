6e2ee212326ac2b3fa0affac8d0d53b456a08496
Saadiq71 <cloudnplacement@gmail.com>
2025-07-12 21:07:31 +0530

    Delete MongoDB + Spring directory

--- MongoDB + Spring/booktrack/src/main/java/library/booktrack/Repos/Bookrepo.java
+++
@@ -1,8 +0,0 @@
-package library.booktrack.Repos;
-
-import library.booktrack.Model.Book;
-import library.booktrack.Model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Bookrepo extends MongoRepository<Book,Integer> {
-}
